1b07c8d9e4f8cadd0e8ad3c037e267b18c715a63
santiagosayshey <sayheysantiago@proton.me>
2024-05-19 11:55:34 +0930

    Removed UTR Tests

--- tests/regex.py
+++ tests/regex.py
@@ -2,7 +2,6 @@ import sys
 from roku import roku
 from h265verify import h265
 from qxr import qxr
-from utr import utr
 from taoe import taoe
 from ralphy import Ralphy
 # ... import other test functions
@@ -19,7 +18,6 @@ def run_tests():
         ("ROKU", roku),
         ("h265 Verified Groups", h265),
         ("QxR Groups", qxr),
-        ("UTR Groups", utr),
         ("TAoE Groups", taoe),
         ("Ralphy", Ralphy)
 
