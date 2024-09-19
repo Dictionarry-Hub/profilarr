352895661e44a8ec1db0462b4678cf39853e6827
Sam Chau <chaucted@gmail.com>
2024-09-19 12:51:25 +0930

    fix: Diff now checking for differences in origin for incoming changes

--- backend/app/git/status/diff.py
+++ backend/app/git/status/diff.py
@@ -4,10 +4,15 @@ import logging
 
 logger = logging.getLogger(__name__)
 
+
 def get_diff(repo_path, file_path):
     try:
         repo = git.Repo(repo_path)
         branch = repo.active_branch.name
+        remote_branch = f'origin/{branch}'  # Assuming the remote is 'origin'
+
+        # Fetch the latest changes from the remote
+        repo.git.fetch()
 
         # Check if the file is untracked
         untracked_files = repo.untracked_files
@@ -20,10 +25,11 @@ def get_diff(repo_path, file_path):
             if not os.path.exists(os.path.join(repo.working_dir, file_path)):
                 diff = "-Deleted File"
             else:
-                # Get the diff for modified files
-                diff = repo.git.diff(f'HEAD', file_path)
+                # Get the diff between the local and the remote branch
+                diff = repo.git.diff(f'{remote_branch}', file_path)
 
         return diff
     except Exception as e:
-        logger.error(f"Error getting diff for file {file_path}: {str(e)}", exc_info=True)
+        logger.error(f"Error getting diff for file {file_path}: {str(e)}",
+                     exc_info=True)
         raise e
