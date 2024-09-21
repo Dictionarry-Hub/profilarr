e0e1f1fe2ee83d813200ca4d2d5af1433a5144c5
ric866 <94540869+ric866@users.noreply.github.com>
2024-09-21 14:50:08 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,24 +1,28 @@
 import os
-master_radarr_base_url = os.environ.get("MASTER_RADAR_BASEURL", "http://localhost:7878")
-master_radarr_apikey = os.environ.get("MASTER_RADAR_APIKEY", "API_KEY")
-dev_radarr_base_url = os.environ.get("DEV_RADAR_BASEURL", "http://localhost:7887")
-dev_radarr_apikey = os.environ.get("DEV_RADAR_APIKEY", "API_KEY")
+master_radarr_base_url = os.environ.get("MASTER_RADARR_BASEURL", "http://localhost:7878")
+master_radarr_apikey = os.environ.get("MASTER_RADARR_APIKEY", "API_KEY")
+dev_radarr_base_url = os.environ.get("DEV_RADARR_BASEURL", "http://localhost:7887")
+dev_radarr_apikey = os.environ.get("DEV_RADARR_APIKEY", "API_KEY")
+master_sonarr_base_url = os.environ.get("MASTER_SONARR_BASEURL", "http://localhost:8989")
+master_sonarr_apikey = os.environ.get("MASTER_SONARR_APIKEY", "API_KEY")
+dev_sonarr_base_url = os.environ.get("DEV_SONARR_BASEURL", "http://localhost:8998")
+dev_sonarr_apikey = os.environ.get("DEV_SONARR_APIKEY", "API_KEY")
 config_content = f"""
 instances:
   radarr:
     - name: "Master"
       base_url: "{master_radarr_base_url}"
-      api_key: "API_KEY"
+      api_key: "{master_radarr_apikey}"
     - name: "DEV"
       base_url: "{dev_radarr_base_url}"
-      api_key: "API_KEY"
+      api_key: "{dev_radarr_apikey}"
   sonarr:
     - name: "Master"
-      base_url: "http://localhost:8989"
-      api_key: "API_KEY"
+      base_url: "{master_sonarr_base_url}"
+      api_key: "{master_sonarr_apikey}"
     - name: "DEV"
-      base_url: "http://localhost:8998"
-      api_key: "API_KEY"
+      base_url: "{dev_sonarr_base_url}"
+      api_key: "{dev_sonarr_apikey}"
 settings:
   export_path: "./exports"
   import_path: "./imports"
