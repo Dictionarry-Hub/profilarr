2dcf6d14e9a5eeae5f2e69cdb5f3358b29905182
Sam Chau <chaucted@gmail.com>
2025-03-30 04:05:11 +1030

    feat: enhance repository stats handling for private repos

--- frontend/src/components/settings/git/repo/ActiveRepo.jsx
+++ frontend/src/components/settings/git/repo/ActiveRepo.jsx
@@ -37,6 +37,20 @@ const RepoStats = ({repoStats}) => {
     if (!repoStats) {
         return <Loader size={14} className='animate-spin' />;
     }
+    
+    // For private repositories or when stats can't be loaded
+    if (repoStats.isPrivate) {
+        return (
+            <div className='flex items-center space-x-1'>
+                <span className='text-xs px-2 py-0.5 rounded-md bg-gray-700/70 border border-gray-600 text-gray-300 flex items-center'>
+                    <svg xmlns="http://www.w3.org/2000/svg" className="h-3 w-3 mr-1" viewBox="0 0 20 20" fill="currentColor">
+                        <path fillRule="evenodd" d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z" clipRule="evenodd" />
+                    </svg>
+                    Private
+                </span>
+            </div>
+        );
+    }
 
     return (
         <>
